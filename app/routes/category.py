4960df399a4a0fb6e7668c8747f91f9d7bdc38ea
Angel <127281111+AJPuRe03@users.noreply.github.com>
2025-05-07 17:56:18 -0600

    Agregar categorias

--- app/routes/category.py
+++ app/routes/category.py
@@ -10,3 +10,15 @@ def listar_categorias():
     posts = Post.query.all()
     categories = Category.query.all()
     return render_template('categories/listar_categorias.html', posts=posts, categories=categories)
+
+@categories_bp.route('/new', methods=['GET','POST'])
+def add_categoria():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_category = Category(name=name)
+        db.session.add(new_category)
+        db.session.commit()
+        return redirect(url_for('categories.listar_categorias'))
+    
+    categories = Category.query.all()
+    return render_template('categories/crear_categoria.html', categories=categories)
